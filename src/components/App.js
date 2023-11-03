0dae1aaedb6052e8b09470b71158306b27628dbc
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-11-03 22:14:43 +0200

    lesson 197 - displaying progress

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import Error from "./Error";
 import StartScreen from "./StartScreen";
 import Question from "./Question";
 import NextButton from "./NextButton";
+import Progress from "./Progress";
 
 const initialState = {
     questions: [],
@@ -40,6 +41,7 @@ function App() {
     const [{questions, status, index, answer, points}, dispatch] = useReducer(reducer, initialState);
 
     const numQuestions = questions.length;
+    const maxPossiblePoints = questions.reduce((prev, curr) => prev + curr.points, 0);
 
     useEffect(function () {
         fetch('http://localhost:8000/questions')
@@ -58,12 +60,22 @@ function App() {
                 {status === 'ready' && <StartScreen numQuestions={numQuestions} dispatch={dispatch}/>}
                 {status === 'active' && (
                     <>
+                        <Progress
+                            index={index}
+                            numQuestions={numQuestions}
+                            points={points}
+                            maxPossiblePoints={maxPossiblePoints}
+                            answer={answer}
+                        />
                         <Question
                             question={questions[index]}
                             dispatch={dispatch}
                             answer={answer}
                         />
-                        <NextButton dispatch={dispatch} answer={answer}/>
+                        <NextButton
+                            dispatch={dispatch}
+                            answer={answer}
+                        />
                     </>
 
                 )}
