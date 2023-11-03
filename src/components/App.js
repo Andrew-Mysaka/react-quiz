6391888f7bf3f41ff53ab09e19e331b6a06e7f16
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-11-03 21:56:25 +0200

    lesson 196 - moving to the next question

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
 import Question from "./Question";
+import NextButton from "./NextButton";
 
 const initialState = {
     questions: [],
@@ -28,6 +29,8 @@ function reducer(state, action) {
             const question = state.questions.at(state.index);
             return {...state, answer: action.payload,
                 points: action.payload === question.correctOption ? state.points + question.points : state.points};
+        case 'nextQuestion':
+            return {...state, index: state.index + 1, answer: null};
         default:
             throw new Error("Actions unknown");
     }
@@ -53,11 +56,17 @@ function App() {
                 {status === 'loading' && <Loader/>}
                 {status === 'error' && <Error/>}
                 {status === 'ready' && <StartScreen numQuestions={numQuestions} dispatch={dispatch}/>}
-                {status === 'active' && <Question
-                    question={questions[index]}
-                    dispatch={dispatch}
-                    answer={answer}
-                />}
+                {status === 'active' && (
+                    <>
+                        <Question
+                            question={questions[index]}
+                            dispatch={dispatch}
+                            answer={answer}
+                        />
+                        <NextButton dispatch={dispatch} answer={answer}/>
+                    </>
+
+                )}
             </Main>
         </div>
     );
