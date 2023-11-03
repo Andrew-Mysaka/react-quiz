9aef0bc4394a367491b286a4c44bdf58d21295df
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-11-03 20:28:23 +0200

    lesson 194 - displaying questions

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,7 @@ const initialState = {
 
     // 'loading', 'error', 'ready', 'active', 'finished'
     status: 'loading',
+    index: 0,
 };
 
 function reducer(state, action) {
@@ -27,7 +28,7 @@ function reducer(state, action) {
 }
 
 function App() {
-    const [{questions, status}, dispatch] = useReducer(reducer, initialState);
+    const [{questions, status, index}, dispatch] = useReducer(reducer, initialState);
 
     const numQuestions = questions.length;
 
@@ -46,7 +47,7 @@ function App() {
                 {status === 'loading' && <Loader/>}
                 {status === 'error' && <Error/>}
                 {status === 'ready' && <StartScreen numQuestions={numQuestions} dispatch={dispatch}/>}
-                {status === 'active' && <Question/>}
+                {status === 'active' && <Question question={questions[index]}/>}
             </Main>
         </div>
     );
---
+++ src/components/Question.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Options from "./Options";
+
+function Question({question}) {
+    return (
+        <div>
+            <h4>{question.question}</h4>
+
+            <Options question={question}/>
+        </div>
+    );
+}
+
+export default Question;
