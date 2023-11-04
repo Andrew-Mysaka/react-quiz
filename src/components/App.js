4c15bc61a55465814924cd3b798dc2272a54bd0b
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-11-04 20:00:04 +0200

    creating quizContext for accessing to questions

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,3 @@
-import {useEffect, useReducer} from "react";
-
 import Header from "./Header";
 import Main from "./Main";
 import Loader from "./Loader";
@@ -8,80 +6,39 @@ import StartScreen from "./StartScreen";
 import Question from "./Question";
 import NextButton from "./NextButton";
 import Progress from "./Progress";
-import FinishScreen from "./FinishScreen";
-import Footer from "./Footer";
+import FinishScree from "./FinishScreen";
 import Timer from "./Timer";
+import Footer from "./Footer";
+import { useQuiz } from "./contexts/QuizContext";
 
-const SECS_REP_QUESTIONS = 30;
-
-const initialState = {
-    questions: [],
-
-    // 'loading', 'error', 'ready', 'active', 'finished'
-    status: 'loading',
-    index: 0,
-    answer: null,
-    points: 0,
-    highscore: 0,
-    secondsRemaining: null
-};
-
-function reducer(state, action) {
-    switch (action.type) {
-        case 'dataReceived':
-            return {...state, questions: action.payload, status: 'ready'};
-        case 'dataFailed':
-            return {...state, status: 'error'};
-        case 'start':
-            return {...state, status: 'active', secondsRemaining: state.questions.length * SECS_REP_QUESTIONS};
-        case 'newAnswer':
-            const question = state.questions.at(state.index);
-            return {...state, answer: action.payload,
-                points: action.payload === question.correctOption ? state.points + question.points : state.points};
-        case 'nextQuestion':
-            return {...state, index: state.index + 1, answer: null};
-        case 'finish':
-            return {...state, status: 'finished',
-                highscore: state.points > state.highscore
-                    ? state.points
-                    : state.highscore
-            };
-        case 'restart':
-            return {...initialState, questions: state.questions, status: 'ready'};
-        case 'tick':
-            return {...state, secondsRemaining: state.secondsRemaining - 1,
-                status: state.secondsRemaining === 0 ? "finished" : state.status,
-                highscore: state.points > state.highscore
-                    ? state.points
-                    : state.highscore
-            };
-        default:
-            throw new Error("Actions unknown");
-    }
-}
-
-function App() {
-    const [{questions, status, index, answer, points, highscore, secondsRemaining}, dispatch] = useReducer(reducer, initialState);
+export default function App() {
+    const {
+        questions,
+        highscore,
+        secondsRemaining,
+        status,
+        points,
+        answer,
+        index,
+        dispatch,
+    } = useQuiz();
 
     const numQuestions = questions.length;
-    const maxPossiblePoints = questions.reduce((prev, curr) => prev + curr.points, 0);
-
-    useEffect(function () {
-        fetch('http://localhost:8000/questions')
-            .then(res => res.json())
-            .then(data => dispatch({type: 'dataReceived', payload: data}))
-            .catch(err => dispatch({type: 'dataFailed'}));
-    }, [])
+    const maxPossiblePoints = questions.reduce(
+        (prev, cur) => prev + cur.points,
+        0
+    );
 
     return (
-        <div className={'app'}>
-            <Header/>
-
+        <div className="app">
+            <Header />
             <Main>
-                {status === 'loading' && <Loader/>}
-                {status === 'error' && <Error/>}
-                {status === 'ready' && <StartScreen numQuestions={numQuestions} dispatch={dispatch}/>}
-                {status === 'active' && (
+                {status === "loading" && <Loader />}
+                {status === "error" && <Error />}
+                {status === "ready" && (
+                    <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+                )}
+                {status === "active" && (
                     <>
                         <Progress
                             index={index}
@@ -96,7 +53,7 @@ function App() {
                             answer={answer}
                         />
                         <Footer>
-                            <Timer dispatch={dispatch} secondsRemaining={secondsRemaining}/>
+                            <Timer dispatch={dispatch} secondsRemaining={secondsRemaining} />
                             <NextButton
                                 dispatch={dispatch}
                                 answer={answer}
@@ -106,16 +63,15 @@ function App() {
                         </Footer>
                     </>
                 )}
-                {status === 'finished' &&
-                <FinishScreen
-                    points={points}
-                    maxPossiblePoints={maxPossiblePoints}
-                    highscore={highscore}
-                    dispatch={dispatch}
-                />}
+                {status === "finished" && (
+                    <FinishScree
+                        points={points}
+                        maxPossiblePoints={maxPossiblePoints}
+                        highscore={highscore}
+                        dispatch={dispatch}
+                    />
+                )}
             </Main>
         </div>
     );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./components/App";
+import { QuizProvider } from "./components/contexts/QuizContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <QuizProvider>
+            <App />
+        </QuizProvider>
+    </React.StrictMode>
 );
 
